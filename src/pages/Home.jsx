ee68492784cd21774539d1e1c2d4a0ef1b62bd75
KarimJK <karimbey211@gmail.com>
2024-02-22 15:38:10 +0500

    feat/homeda card yaratildi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
-
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      {" "}
+      <div className="card text-white bg-blue-950 w-[500px] h-[300px] m-auto mt-20 p-5 flex items-center justify-center rounded-lg">
+        <h1>Card part</h1>
+
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
